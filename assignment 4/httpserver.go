973960589e3beeb3de4511db9b600aec6882aa35
Meenakshi Viswom <meenakshiviswom@gmail.com>
2021-01-08 13:14:43 +0530

    commit

--- assignment 4/httpserver.go
+++ assignment 4/httpserver.go
@@ -1,16 +1,18 @@
 package main
+
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
+
+func handler(W http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(W, "Welcome to my home page\n", r.URL.Path[1:])
+}
+func handler2(W http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(W, "hello world\n")
+}
 func main() {
 	http.HandleFunc("/", handler)
-	http.HandleFunc("/Hello", handler2)
-	http.ListenAndServe(":8080", nil)
-	}
-	func handler (W http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(W, "Welcome to my home page\n")
-	}
-	func handler2 (W http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(W, "hello world\n")
-	}
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
